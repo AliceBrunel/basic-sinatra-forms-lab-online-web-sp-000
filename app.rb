47efa27f4e2489f2e18c89422bdb72744ee2daad
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 11:42:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,11 +11,6 @@ class App < Sinatra::Base
     @power_forward = params[:power_forward]
     @center = params[:center]
     
-    <p>Point Guard:<input type="text" name="point"></p>
-    <p>Shooting Guard:<input type="text" name="shooting"></p>
-    <p>Small Forward:<input type="text" name="small_forward"></p>
-    <p>Power Forward:<input type="text" name="power_forward"></p>
-    <p>Center:<input type="text" name="center"></p>
     
     erb :newteam
   end
