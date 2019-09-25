0ebacc09f159bca1a91f274e15833066c35be83d
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 11:42:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,18 @@ class App < Sinatra::Base
 
   get '/newteam' do 
     @team = params[:team]
-    @coach = params[:team]
-    @point = params[:team]
-    @shooting = params[:team]
-    @small_forward = params[:team]
-    @power_forward = params[:team]
-    @center = params[:team]
+    @coach = params[:coach]
+    @point = params[:point]
+    @shooting = params[:shooting]
+    @small_forward = params[:small_forward]
+    @power_forward = params[:power_forward]
+    @center = params[:center]
+    
+    <p>Point Guard:<input type="text" name="point"></p>
+    <p>Shooting Guard:<input type="text" name="shooting"></p>
+    <p>Small Forward:<input type="text" name="small_forward"></p>
+    <p>Power Forward:<input type="text" name="power_forward"></p>
+    <p>Center:<input type="text" name="center"></p>
     
     erb :newteam
   end
