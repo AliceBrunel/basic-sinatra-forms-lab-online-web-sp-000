61265caa6d64e449ee12d9c8a1887f9f96086b2b
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 11:51:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     @coach = params[:coach]
     @point = params[:pg]
     @shooting = params[:sg]
-    @small_forward = params[:small_forward]
+    @small_forward = params[:sf]
     @power_forward = params[:pf]
     @center = params[:c]
     
