4144719a887f480b3b055f5bdcc8a187b5d9da5d
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 11:41:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ class App < Sinatra::Base
 
   get '/newteam' do 
     @team = params[:team]
-    @coach = 
+    @coach = params[:team]
+    @point = params[:team]
+    @shooting = params[:team]
+    @small_forward = params[:team]
+    @power_forward = params[:team]
+    @center = params[:team]
+    
     erb :newteam
   end
 
